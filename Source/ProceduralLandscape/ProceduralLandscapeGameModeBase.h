1da2f576c9692adc4ea1882f49ca64143af32379
Bastian Lang <bluefruitgamesofficial@gmail.com>
2022-04-03 18:36:20 +0200

    Init

---
+++ Source/ProceduralLandscape/ProceduralLandscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProceduralLandscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALLANDSCAPE_API AProceduralLandscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
