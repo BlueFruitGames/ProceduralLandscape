5c1e135775c933c5f10a87c2d8f17502adb83c17
Bastian Lang <bluefruitgamesofficial@gmail.com>
2022-04-06 13:06:36 +0200

    Apply Z-Noise to the tile

--- Source/ProceduralLandscape/Tile/ProceduralTile.cpp
+++ Source/ProceduralLandscape/Tile/ProceduralTile.cpp
@@ -15,6 +15,12 @@ AProceduralTile::AProceduralTile()
 	SetRootComponent(ProceduralMeshComponent);
 }
 
+void AProceduralTile::OnConstruction(const FTransform& Transform) {
+	if (bReloadInEditor) {
+		GenerateTile();
+	}
+}
+
 // Called when the game starts or when spawned
 void AProceduralTile::BeginPlay()
 {
@@ -40,39 +46,81 @@ void AProceduralTile::GenerateTile() {
 
 	for (int Row = 0; Row < TileResolution; ++Row) {
 		for (int Column = 0; Column < TileResolution; ++Column) {
+
 			float CurrentXOffset = StartOffset - DistanceBetweenVertices * Row;
 			float CurrentYOffset = StartOffset - DistanceBetweenVertices * Column;
-			Vertices.Add(FVector(CurrentXOffset, CurrentYOffset, 0));
 
-			if (Column < TileResolution - 1 && Column < TileResolution - 1) {
+			float UPos = MapToUV(CurrentXOffset, StartOffset);
+			float VPos = MapToUV(CurrentYOffset, StartOffset);
+
+			float CurrentZOffset = GetZOffset(UPos, VPos, StartOffset);
+
+			FVector CurrentLocation(CurrentXOffset, CurrentYOffset, CurrentZOffset);
+			Vertices.Add(CurrentLocation);
+
+			if (Row < TileResolution - 1 && Column < TileResolution - 1) {
 				GenerateTriangles(Triangles, Row, Column);
 			}
-
-			Normals.Add(FVector(0, 0, 1));
-
-			float UPos = (CurrentXOffset + StartOffset) / TileSize;
-			float VPos = (CurrentYOffset + StartOffset) / TileSize;
+			Normals.Add(CalculateVertexNormal(CurrentXOffset, CurrentYOffset, StartOffset, DistanceBetweenVertices));
 			UV0.Add(FVector2D(UPos, VPos));
+			VertexColor.Add(FLinearColor(CurrentZOffset, 1 - CurrentZOffset, 0));
 		}
 	}
-
-	ProceduralMeshComponent->CreateMeshSection_LinearColor(0, Vertices, Triangles, Normals, UV0, TArray<FLinearColor>(), TArray<FProcMeshTangent>(), true);
+	ProceduralMeshComponent->CreateMeshSection_LinearColor(0, Vertices, Triangles, Normals, UV0, VertexColor, TArray<FProcMeshTangent>(), true);
 }
+
 
 void AProceduralTile::GenerateTriangles(TArray<int32>& Triangles, int CurrentRow, int CurrentColumn) {
 	int Current = (CurrentRow * TileResolution) + CurrentColumn;
 	int Right = Current + 1;
 	int Lower = Current + TileResolution;
 	int LowerRight = Lower + 1;
-	AddTriangles(Triangles, Right, LowerRight, Current);
-	AddTriangles(Triangles, Lower, Current, LowerRight);
+	AddTriangle(Triangles, Right, LowerRight, Current);
+	AddTriangle(Triangles, Lower, Current, LowerRight);
 }
 
-void AProceduralTile::AddTriangles(TArray<int32>& Triangles, int32 V1, int32 V2, int32 V3) {
+void AProceduralTile::AddTriangle(TArray<int32>& Triangles, int32 V1, int32 V2, int32 V3) {
 	Triangles.Add(V1);
 	Triangles.Add(V2);
 	Triangles.Add(V3);
 }
 
+FVector AProceduralTile::CalculateVertexNormal(float XPos, float YPos, float Offset, float DistanceBetweenVertices) {
+	float Top = MapToUV(YPos + DistanceBetweenVertices, Offset);
+	float Bottom = MapToUV(YPos - DistanceBetweenVertices, Offset);
+	float Right = MapToUV(XPos + DistanceBetweenVertices, Offset);
+	float Left = MapToUV(XPos - DistanceBetweenVertices, Offset);
 
+	float ZTopLeft = GetZOffset(Left, Top, Offset);
+	float ZTopRight = GetZOffset(Right, Top, Offset);
+	float ZBottomLeft = GetZOffset(Left, Bottom, Offset);
+	float ZBottomRight = GetZOffset(Right, Bottom, Offset);
+
+	FVector TopLeftLocation(XPos - DistanceBetweenVertices, YPos + DistanceBetweenVertices, ZTopLeft);
+	FVector TopRightLocation(XPos + DistanceBetweenVertices, YPos + DistanceBetweenVertices, ZTopRight);
+	FVector BottomLeftLocation(XPos - DistanceBetweenVertices, YPos - DistanceBetweenVertices, ZBottomLeft);
+	FVector BottomRightLocation(XPos + DistanceBetweenVertices, YPos - DistanceBetweenVertices, ZBottomRight);
+
+
+	FVector Normal_01 = FVector::CrossProduct((BottomLeftLocation - TopLeftLocation), (TopRightLocation - TopLeftLocation));
+	FVector Normal_02 = FVector::CrossProduct((BottomRightLocation - BottomLeftLocation), (TopRightLocation - BottomLeftLocation));
+	return (Normal_02 + Normal_01 / 2).GetSafeNormal();
+}
+
+float AProceduralTile::GetZOffset(float XPos, float YPos, float Offset) {
+
+	float ScaledXPos = MapToRange(XPos, 0, 1, 0, NoiseScale.X);
+	float ScaledYPos = MapToRange(YPos, 0, 1, 0, NoiseScale.Y);
+
+	float ZOffset = FMath::PerlinNoise2D(FVector2D(ScaledXPos + NoiseOffset.X, ScaledYPos + NoiseOffset.Y)) * NoiseStrength;
+	return ZOffset;
+}
+
+float AProceduralTile::MapToUV(float Pos, float Offset) {
+	return (Pos + Offset) / TileSize;
+}
+
+float AProceduralTile::MapToRange(float Value, float MinPrev, float MaxPrev, float MinNew, float MaxNew) {
+	return MinNew + (Value - MinPrev) * (MaxNew - MinNew) / (MaxPrev - MinPrev);
+}
 
--- Source/ProceduralLandscape/Tile/ProceduralTile.h
+++ Source/ProceduralLandscape/Tile/ProceduralTile.h
@@ -10,7 +10,7 @@ UCLASS()
 class PROCEDURALLANDSCAPE_API AProceduralTile : public AActor
 {
 	GENERATED_BODY()
-	
+
 public:
 	// Sets default values for this actor's properties
 	AProceduralTile();
@@ -19,24 +19,52 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	UPROPERTY(EditAnywhere, meta = (UIMin = 1))
-		int TileSize;
+	bool bReloadInEditor;//Should the mesh be updated in the editor if changes are made to it's properties?
+
+	UPROPERTY(EditAnywhere, meta = (UIMin = 1))
+	int TileSize; //Width of the tile
 
 	UPROPERTY(EditAnywhere, meta = (UIMin = 2, UIMax = 100))
-		int TileResolution;
+	int TileResolution;//Count of vertices on each axis
+
+	UPROPERTY(EditAnywhere, meta = (UIMin = 0))
+	int NoiseStrength; //The influence of the PerlinNoise
+
+	UPROPERTY(EditAnywhere)
+	FVector2D NoiseOffset; //X and Y offset of the PerlinNoise
+
+	UPROPERTY(EditAnywhere)
+	FVector2D NoiseScale;//X and Y scale of the PerlinNoise
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	virtual void OnConstruction(const FTransform& Transform) override;
+
 private:
 	UPROPERTY()
-		class UProceduralMeshComponent* ProceduralMeshComponent;
+	class UProceduralMeshComponent* ProceduralMeshComponent;
 
+	//Procedurally generates a tile using ProceduralMeshComponent
 	UFUNCTION(CallInEditor)
-		void GenerateTile();
+	void GenerateTile();
 
+	//Generates the triangles vor the vertex at (CurrentRow, CurrentColumn)
 	void GenerateTriangles(TArray<int32>& Triangles, int CurrentRow, int CurrentColumn);
 
-	void AddTriangles(TArray<int32>& Triangles, int32 V1, int32 V2, int32 V3);
+	//Adds a triangle for the Vertices V1, V2, V3
+	void AddTriangle(TArray<int32>& Triangles, int32 V1, int32 V2, int32 V3);
 
+	//Calculates the normal of vertex at Position (XPos, YPos)
+	FVector CalculateVertexNormal(float XPos, float YPos, float Offset, float DistanceBetweenVertices);
+
+	// Maps a value from range (MinPrev,MaxPrev) to the value in the new range (MinNew, MaxNew)
+	float MapToRange(float Value, float MinPrev, float MaxPrev, float MinNew, float MaxNew);
+
+	//Caclulates the Z-Offset at Position(XPos,YPos) using the perlin noise function
+	float GetZOffset(float XPos, float YPos, float Offset);
+
+	//Maps a position to the UV-Space
+	float MapToUV(float Pos, float Offset);
 };
